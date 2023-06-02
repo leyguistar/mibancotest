3e173266e291e17f39ee9d7019f4807812639671
David <luigisperstar7@gmail.com>
2023-06-02 09:25:48 -0400

    remove not used imports

--- app.py
+++ app.py
@@ -8,28 +8,6 @@ from time import time
 import json
 app = Flask(__name__)
 cors = CORS(app)
-with open('gpt/data/products.csv','r') as h:
-    data = h.read()
-
-def manageMessage(sender,message,conversationId):
-    if(message['type'] == 'text'):
-        print(sender + ': ',message['text']['body'])
-        r = getResponse(sender,message['text']['body'],data,conversationId)
-
-        print('response: ',r)
-        send_message(r)
-    else:
-        print(sender + ': ',message)
-
-def manageBusiness(data):
-    for entry in data['entry']:
-        for change in entry['changes']:
-            if(change['field'] == 'messages'):
-                messages = change['value'].get('messages',[])
-                if(messages):
-                    sender = change['value']['contacts'][0]['profile']['name']
-                    for message in messages:
-                        manageMessage(sender,message,conversationId = entry['id'])
 def generateResponse(result):
     response = {
         "sucess":result
